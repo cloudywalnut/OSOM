b3a183e3394999ad33ff68e94b6cfac2c137efb1
Mustansir Muhammad <tp078682@mail.apu.edu.my>
2025-03-22 17:00:05 +0800

    Google Endpoint added

--- app.py
+++ app.py
@@ -10,14 +10,19 @@ client = OpenAI(
   api_key=os.getenv('apiKey')
 )
 
+clientGoogle = OpenAI(
+    api_key = os.getenv('apiKey-google'),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
 
 @app.route('/')
 def home():
     return 'Server is Running'
 
 
-@app.route('/chat', methods=['POST'])
-def chat():
+@app.route('/chat/openai', methods=['POST'])
+def chatOpenai():
     data = request.json
     message = data.get("message", "")
     if not message:
@@ -34,6 +39,28 @@ def chat():
     )
 
     return jsonify({"response": response.choices[0].message.content})
+
+
+@app.route('/chat/google', methods=['POST'])
+def chatGoogle():
+    data = request.json
+    message = data.get("message", "")
+    if not message:
+        return jsonify({"error": "No message provided"}), 400
+
+    response = clientGoogle.chat.completions.create(
+        model="gemini-2.0-flash-lite",
+        n=1,
+        messages=[
+            {"role": "user", "content": message},
+            {"role": "system", "content": "Please act like a friend to a child and use very simple easy tone to teach him the basics of the solar system once he/she starts the conversation"}
+        ],
+        max_tokens=100,
+        temperature= 0.7
+    )
+
+    return jsonify({"response": response.choices[0].message.content})
+
 
 if __name__ == "__main__":  
     port = int(os.getenv("PORT", 5000))  # Uses Railway's port, defaults to 5000 if running locally
