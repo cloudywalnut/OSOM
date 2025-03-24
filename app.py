697075632d155607b8863d4d04975ca89c41db2e
Mustansir Muhammad <tp078682@mail.apu.edu.my>
2025-03-24 15:51:05 +0800

    CORS Added

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request, render_template
 from openai import OpenAI
 from dotenv import load_dotenv
+from flask_cors import CORS
 import markdown
 load_dotenv()
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 client = OpenAI(
   api_key=os.getenv('apiKey')
