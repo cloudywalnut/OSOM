954c87f8d74ce3a7d0348b728c4a7b728f2e3f46
Mustansir Muhammad <tp078682@mail.apu.edu.my>
2025-03-26 16:07:51 +0800

    WebSocket Added

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify, request, render_template
 from openai import OpenAI
 from dotenv import load_dotenv
 from flask_cors import CORS
+from flask_socketio import SocketIO
 import markdown
 load_dotenv()
 import os
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 client = OpenAI(
   api_key=os.getenv('apiKey')
@@ -65,6 +67,39 @@ def chatGoogle():
     return jsonify({"response": markdown.markdown(response.choices[0].message.content)})
 
 
+
+# Provider of Emotions
+@socketio.on('message')
+def message_emotion(message):
+
+    response_message = clientGoogle.chat.completions.create(
+        model="gemini-2.0-flash-lite",
+        n=1,
+        messages=[
+            {"role": "system", "content": "Please act like a friend to a child and use very simple easy tone to teach him the basics of the solar system once he/she starts the conversation, Don't start with a greeting"},
+            {"role": "user", "content": message}
+        ],
+        max_tokens=100,
+        temperature= 0.7
+    )
+
+    socketio.emit("response", markdown.markdown(response_message.choices[0].message.content))
+
+    response_emotion = clientGoogle.chat.completions.create(
+        model="gemini-2.0-flash-lite",
+        n=1,
+        messages=[
+            {"role": "system", "content": "Understand the emotion of the message and return exactly just one word from: Happy, Sad, Angry, Neutral, Excited."},
+            {"role": "user", "content": response_message.choices[0].message.content[:20]}
+        ],
+        max_tokens=2,
+        temperature=0.7
+    )
+
+    socketio.emit("emotion", markdown.markdown(response_emotion.choices[0].message.content))
+
+
+
 if __name__ == "__main__":  
     port = int(os.getenv("PORT", 5000))  # Uses Railway's port, defaults to 5000 if running locally
-    app.run(host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
