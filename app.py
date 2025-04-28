d7989c22312ef55d36bdfa417f959549189f61ee
Mustansir Muhammad <tp078682@mail.apu.edu.my>
2025-04-28 22:17:19 +0800

    incomplete answer fix

--- app.py
+++ app.py
@@ -36,10 +36,9 @@ def chatOpenai():
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "Please act like a friend to a child and use very simple easy tone to teach him the basics of the solar system once he/she starts the conversation. Strictly always stick to topic and do not use emojis"},
+            {"role": "system", "content": "Please act like a friend to a child and use very simple easy tone to teach him the basics of the solar system once he/she starts the conversation. Strictly always stick to topic and do not use emojis, keeping the response short and on point"},
             {"role": "user", "content": message}
         ],
-        max_tokens=100,
         temperature= 0.7
     )
 
